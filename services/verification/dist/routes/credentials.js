9daf3030c7ebca7ec1365cda7eed1bf32f6e2690
Bipin J John <bipin.john@7edge.com>
2025-10-08 23:29:41 +0530

    removed the dist and added in gitignore

--- services/verification/dist/routes/credentials.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const verify_1 = require("../services/credentials/handlers/verify");
-const router = (0, express_1.Router)();
-router.post('/credentials', verify_1.verifyCredential);
-exports.default = router;
-//# sourceMappingURL=credentials.js.map
--- services/verification/dist/services/credentials/handlers/verify.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response } from 'express';
-export declare const verifyCredential: (req: Request, res: Response) => Promise<Response<any, Record<string, any>> | undefined>;
-//# sourceMappingURL=verify.d.ts.map
